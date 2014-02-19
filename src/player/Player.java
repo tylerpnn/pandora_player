499ea71549da28847621c4a175ab2a39e090b8b6
Tyler Pennington <tylerpnn@gmail.com>
2014-02-18 19:06:35 -0500

    streaming kinda working, not really

--- src/player/Player.java
+++ src/player/Player.java
@@ -1,26 +1,17 @@
 package player;
 
-import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.List;
 
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Control;
 import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.SourceDataLine;
 
 import json.response.StationListResponse.Result.StationInfo;
-import net.sourceforge.jaad.aac.Decoder;
-import net.sourceforge.jaad.aac.Profile;
-import net.sourceforge.jaad.aac.SampleBuffer;
-import net.sourceforge.jaad.mp4.MP4Container;
-import net.sourceforge.jaad.mp4.api.AudioTrack;
-import net.sourceforge.jaad.mp4.api.Frame;
-import net.sourceforge.jaad.mp4.api.Movie;
-import net.sourceforge.jaad.mp4.api.Track;
+import net.sourceforge.jaad.spi.javasound.AACAudioFileReader;
 import pandora.Application;
 import pandora.Song;
 import pandora.UserSession;
@@ -138,9 +129,12 @@ public class Player {
 				con = url.openConnection();
 				dis = new DataInputStream(con.getInputStream());
 				data = new byte[con.getContentLength()];
+				System.out.println(con.getContentLength());
 				for(int i=0; i < data.length; i++) {
 					data[i] = dis.readByte();
+					System.out.printf("i: %d, b: %d\n", i, data[i]);
 				}
+				System.out.println("hi");
 				dis.close();
 			} catch(Exception e) {
 				e.printStackTrace();
@@ -168,32 +162,24 @@ public class Player {
 		
 		private boolean decodeMp4(Song song) {
 			song.setPlaying(true);
-			byte[] songData = getSongData(song);
 			dataLine = null;
 			try {
-				MP4Container cont = new MP4Container(new ByteArrayInputStream(songData));
-				Movie movie = cont.getMovie();
-				song.setDuration((int)movie.getDuration());
-				List<Track> tracks = movie.getTracks(AudioTrack.AudioCodec.AAC);
-				AudioTrack track = (AudioTrack) tracks.get(0);
-				AudioFormat audioFormat = new AudioFormat(track.getSampleRate()/2, track.getSampleSize(), track.getChannelCount(), true, true);
-				dataLine = AudioSystem.getSourceDataLine(audioFormat);
-				dataLine.open(audioFormat);
-				setVolume(volume);
+				String url = song.getSongInfo().getAudioUrlMap().getHighQuality().getAudioUrl();
+				System.out.print(song.getSongInfo().getSongName() + " ");
+				System.out.println(url);
+				AACAudioFileReader aafr = new AACAudioFileReader();
+				AudioInputStream ais = aafr.getAudioInputStream(new URL(url));
+				System.out.printf("%d\n", ais.getFrameLength());
+				dataLine = AudioSystem.getSourceDataLine(ais.getFormat());
+				dataLine.open();
 				dataLine.start();
-				Decoder dec = new Decoder(track.getDecoderSpecificInfo());
-				dec.getConfig().setProfile(Profile.AAC_LC);
-				dec.getConfig().setSBREnabled(false);
-				Frame frame;
-				byte[] chunk;
-				SampleBuffer buf = new SampleBuffer();
-				while(!skip && track.hasMoreFrames()) {
+				byte[] buf = new byte[4096];
+				int bytesRead = 0;
+				while(!skip && bytesRead != -1) {
 					while(!skip && paused) { sleep(100L); }
-					frame = track.readNextFrame();
-					song.update((int)frame.getTime());
-					dec.decodeFrame(frame.getData(), buf);
-					chunk = buf.getData();
-					dataLine.write(chunk, 0, chunk.length);
+					bytesRead = ais.read(buf, 0, buf.length);
+					if(bytesRead != -1)
+						dataLine.write(buf, 0, buf.length);
 				}
 				song.setPlaying(false);
 				skip = false;
