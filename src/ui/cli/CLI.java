53be1ce8005fc77c881f4e9c031993b773b358c2
Tyler Pennington <tylerpnn@gmail.com>
2014-03-09 03:20:40 -0400

    nothing

--- src/ui/cli/CLI.java
+++ src/ui/cli/CLI.java
@@ -1,5 +1,10 @@
 package ui.cli;
 
+import java.io.Console;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
 import pandora.Application;
 import pandora.Song;
 import pandora.UserInterface;
@@ -7,19 +12,25 @@ import pandora.UserInterface;
 public class CLI implements UserInterface {
 
 	private Application app;
+	private Console c;
+	private Reader in;
+	private Writer out;
 	
 	public CLI(Application app) {
 		this.app = app;
-		System.out.println("hi");
-		System.out.println("hi");
-		System.out.println("hi");
-		System.out.println("hi");
-		System.out.println("hi");
-		
+		if((c = System.console()) == null) {
+			throw new RuntimeException("Console not supported");
+		}
+		in = c.reader();
+		out = c.writer();
 	}
 	
 	public void print(String s) {
-		System.out.println(s);
+		try {
+			out.write(s);
+		} catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
 	}
 
 	@Override
